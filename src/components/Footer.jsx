3b48960d689ffd5e009aad2b2a6da3d65d2c24c0
Neragoro <neragoro95@gmail.com>
2024-04-07 14:43:23 -0400

    task done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Recipe App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
+
